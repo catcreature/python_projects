cb1a994d6324b4548cb1c5f78e9d65c17c092479
catcreature <157812154+catcreature@users.noreply.github.com>
2025-01-24 01:36:41 +0530

    countdown timer (#23)

---
+++ 11-Countdown Timer/main.py
@@ -0,0 +1,12 @@
+import time
+def countdown(t):
+    while t:
+        minutes, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print('Fire in the hole!!')
+
+t = input("Enter the time in seconds: ")
+countdown(int(t))
